567864f7ed06e30f869dd4ae8d371145cbcee84a
Alexander <39242099+GoodBroch@users.noreply.github.com>
2020-04-10 23:24:28 +0300

    Laba-6
    & added

--- Lab-6/Predicats.h
+++ Lab-6/Predicats.h
@@ -1,6 +1,6 @@
 #pragma once
 
-template <typename T> bool none_of(T& start,const T& stop, bool (*predicate)(T param)) {
+template <typename T> bool none_of(T& start,const T& stop, bool (*predicate)(T& param)) {
     while (start != stop) {
         if (predicate(start))
             return false;
@@ -9,7 +9,7 @@ template <typename T> bool none_of(T& start,const T& stop, bool (*predicate)(T p
     return !predicate(start);
 }
 
-template <typename T> bool is_sorted(T* start,const T* stop, bool (*predicate)(T first, T second)) {
+template <typename T> bool is_sorted(T* start,const T* stop, bool (*predicate)(T& first, T& second)) {
     while (start != stop) {
         if (!predicate(*start, *(start + 1)))
             return false;
@@ -18,7 +18,7 @@ template <typename T> bool is_sorted(T* start,const T* stop, bool (*predicate)(T
      return true;
 }
 
-template <typename T> T* find_backward(const T& start, T& stop, bool (*predicate)(T first)) {
+template <typename T> T* find_backward(const T& start, T& stop, bool (*predicate)(T& first)) {
     while (start != stop) {
         if (predicate(stop))
             return &stop;
